a84c53668a81721d40856cf45705eb363d1056c6
Simardeep Singh <simardeepsngh1@gmail.com>
2021-03-22 09:06:47 -0400

    Network Sniffer Added

---
+++ Networking - Sockets and Sniffing/Packet_Sniffing/main.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+import socket
+import os
+
+# Host to listen
+host = "127.0.0.1"
+
+# Creating Raw Socket and bind it to public interface
+if os.name == "nt":
+    socket_protocol = socket.IPPROTO_IP
+else:
+    socket_protocol = socket.IPPROTO_ICMP
+
+sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+
+sniffer.bind((host, 0))
+
+# We want the IP headers to include in the capture
+sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+# If we are using windows, we need to send a IOCTL to set up promiscuous mode
+if os.name == 'nt':
+    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+# Read in singple packet
+print(sniffer.recvfrom(65565))
+
+# Turn of promiscuois mode when using windows
+if os.name == 'nt':
+    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
